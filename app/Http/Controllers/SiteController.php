7288839bc053eb32d96e7e63cd680e7c15bc3ace
MaxV91 <max.kotsurenko@gmail.com>
2017-02-11 13:41:55 +0300

    lesson 5

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,10 +2,12 @@
 
 namespace Corp\Http\Controllers;
 
-use Corp\Repositories\MenusRepository;
 use Illuminate\Http\Request;
 
 use Corp\Http\Requests;
+use Corp\Repositories\MenusRepository;
+
+use Menu;
 
 class SiteController extends Controller
 {
@@ -32,7 +34,7 @@ class SiteController extends Controller
     {
         $menu = $this->getMenu();
 
-        $navigation = view(env('THEME').'.navigation')->render();
+        $navigation = view(env('THEME') . '.navigation')->with('menu', $menu)->render();
         $this->vars = array_add($this->vars, 'navigation', $navigation);
 
         return view($this->template)->with($this->vars);
@@ -42,7 +44,23 @@ class SiteController extends Controller
     {
         $menu = $this->m_rep->get();
 
-        return $menu;
+
+        $mBuilder = Menu::make('MyNav', function ($m) use ($menu) {
+
+            foreach ($menu as $item) {
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)->id($item->id);
+                } else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)->add($item->title, $item->path)->id($item->id);
+                    }
+                }
+            }
+        });
+
+        //dd($mBuilder);
+
+        return $mBuilder;
     }
 
 }
