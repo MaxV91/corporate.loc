1e8580e06d5569a87c0162b87f0325d9bf2c857d
MaxV91 <max.kotsurenko@gmail.com>
2017-02-13 16:44:10 +0300

    lesson 8

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,9 @@ Route::resource('/', 'IndexController', [
                             'only' => 'index',
                             'names' => ['index' => 'home']
                         ]);
+
+Route::resource('portfolios', 'PortfolioController', [
+                        'parameters' => [
+                            'portfolios' => 'alias'
+                        ]
+]);
