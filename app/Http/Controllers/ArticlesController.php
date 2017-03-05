786decef0dbaa2d3e12c5cb46d793e25ba959e5a
MaxV91 <max.kotsurenko@gmail.com>
2017-03-05 23:26:51 +0300

    lesson 22

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -28,6 +28,10 @@ class ArticlesController extends SiteController
 
     public function index($cat_alias = FALSE)
     {
+        $this->title = 'Блог';
+        $this->keywords = 'String';
+        $this->meta_desc = 'String';
+
         $articles = $this->getArticles($cat_alias);
 
         $content = view(env('THEME').'.articles_content')->with('articles', $articles)->render();
@@ -70,7 +74,7 @@ class ArticlesController extends SiteController
             $where = ['category_id', $id];
         }
 
-        $articles = $this->a_rep->get(['id', 'title', 'alias', 'created_at', 'img', 'desc', 'user_id', 'category_id'], FALSE, TRUE, $where);
+        $articles = $this->a_rep->get(['id', 'title', 'alias', 'created_at', 'img', 'desc', 'user_id', 'category_id', 'keywords', 'meta_desc'], FALSE, TRUE, $where);
 
         if ($articles) {
             $articles->load('user', 'category', 'comments');
@@ -86,6 +90,10 @@ class ArticlesController extends SiteController
         if ($article) {
             $article->img = json_decode($article->img);
         }
+
+        $this->title = $article->title;
+        $this->keywords = $article->keywords;
+        $this->meta_desc = $article->meta_desc;
 
 
         $content = view(env('THEME').'.article_content')->with('article', $article)->render();
