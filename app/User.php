81735aabb916bf7903379af8bc6f317a08911c3e
MaxV91 <max.kotsurenko@gmail.com>
2017-03-23 23:33:58 +0300

    lesson 30

--- app/User.php
+++ app/User.php
@@ -33,4 +33,58 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('Corp\Role', 'role_user');
     }
+
+    public function canDo($permission, $require = FALSE)
+    {
+        if (is_array($permission)) {
+            foreach ($permission as $permName) {
+                $permName = $this->canDo($permName);
+                if ($permName && !$require) {
+                    return TRUE;
+                }
+                else if (!$permName && $require) {
+                    return FALSE;
+                }
+            }
+
+            return $require;
+
+        }
+        else {
+            foreach ($this->roles as $role) {
+                foreach ($role->perms as $perm) {
+                    if (str_is($permission, $perm->name)) {
+                        return TRUE;
+                    }
+                }
+            }
+        }
+    }
+
+    public function hasRole($name, $require = FALSE)
+    {
+        if (is_array($name)) {
+            foreach ($name as $roleName) {
+                $hasRole = $this->hasRole($roleName);
+
+                if ($hasRole && !$require) {
+                    return TRUE;
+                }
+                else if (!$hasRole && $require) {
+                    return FALSE;
+                }
+            }
+
+            return $require;
+        }
+        else {
+            foreach ($this->roles as $role) {
+                if ($role->name == $name) {
+                    return TRUE;
+                }
+            }
+        }
+
+        return FALSE;
+    }
 }
