d1fd3c8110ab620b8cc5ea8ca1e90b37e017565a
MaxV91 <max.kotsurenko@gmail.com>
2017-04-21 19:21:20 +0300

    lesson 33

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -6,6 +6,7 @@ use Corp\Repositories\ArticlesRepository;
 use Illuminate\Http\Request;
 
 use Corp\Http\Requests;
+use Corp\Http\Requests\ArticleRequest;
 use Corp\Http\Controllers\Controller;
 
 use Gate;
@@ -83,9 +84,15 @@ class ArticlesController extends AdminController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
-        //
+        $result = $this->a_rep->addArticle($request);
+
+        if(is_array($result) && !empty($result['error'])) {
+            return back()->with($result);
+        }
+
+        return redirect('/admin')->with($result);
     }
 
     /**
