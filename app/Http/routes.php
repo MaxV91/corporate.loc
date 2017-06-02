b5e20336549991d8e904e3477cf9e51e6b2818ca
MaxV91 <max.kotsurenko@gmail.com>
2017-06-02 23:27:42 +0300

    lesson 40

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -48,4 +48,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
 
     Route::resource('/articles', 'Admin\ArticlesController');
     Route::resource('/permissions', 'Admin\PermissionsController');
+    Route::resource('/menus', 'Admin\MenusController');
 });
